e872f4c483af772fe1121537b1776f14bf151448
Lilia <liliabendjeddou9@gmail.com>
2023-09-08 03:28:13 +0100

    Assignment done

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    const wordToCompare = word.split("").reverse().join("");
+    return word === wordToCompare ? (true) : (false)
 }
-
-/* 
-  Add your pseudocode here
-*/
+console.log(isPalindrome("madam")); 
+console.log(isPalindrome("robot"));
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution 
+  
+  we compare the original string with its reverse for that we use reverse(), 
+  but we need first to turn the string into an array of characters, 
+  that's why we first use split(). 
+  And then join() to joins the array elements back together into a single string.
+  If the reversed string is the same as the original string, 
+  we return true; otherwise, we return false.
 */
 
 // You can run `node index.js` to view these console logs
